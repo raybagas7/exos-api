36a802cf24cb0db0061b16d34309f42b1f6e96be
raybagas7 <raysam.gas@gmail.com>
2023-06-24 13:50:47 +0700

    Add postgres for relational data, and mongo for realtime data

---
+++ migrations/1687589320335_create-table-users.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    search_id: {
+      type: 'SERIAL',
+      unique: true,
+      notNull: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    profile_name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    profile_img: {
+      type: 'TEXT',
+    },
+  });
+
+  pgm.createExtension('citext');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+  pgm.dropExtension('citext');
+};
